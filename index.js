2c38d6cd4b60b71915c0e5cf59b50b37e3d4e83d
syneva-runyan <syneva@gmail.com>
2019-08-08 21:45:19 -0400

    A simple module used to multiple two round numbers

---
+++ index.js
@@ -0,0 +1,44 @@
+/*
+* The Challenge
+*
+* The purpose of this code is to write an elegant solution to the following request:
+* Multiply any two integers without using the multiplication or division operators.
+*/
+
+/**
+ * @function isInt
+ * @description determine if provided value is an a round number
+ * @param {?} a 
+ * @return {bool} whether or not provided value is a round number
+ */
+function isInt(a) {
+    return Number.isNaN(parseInt(a)) && a % 1 === 0;
+}
+
+/**
+ * @function multiply
+ * @description multiply two round numbers
+ * @param {int} a 
+ * @param {int} b 
+ * @return {int} product
+ */
+const multiply = function(a, b) {
+    // reject non integers
+    if(isInt(a) || isInt(b)) {
+        throw new Error("Please provide only integers to function:multiply");
+    }
+
+    let product = 0;
+    let i = 0; // keep track of iterations during computation
+    while(b > 0) {
+        if (b % 2 == 1) {
+            // bitshift a and add to result.
+            product += a << i;
+        }
+        b = b >> 1;
+        i++;    
+    }
+    return product;
+}
+
+module.exports = multiply;
---
+++ spec/index.spec.js
@@ -0,0 +1,20 @@
+const multiply = require('../index');
+
+describe('multiply', function() {
+    it('should calculate the product of two numbers', function() {
+        expect(multiply(5,2)).toEqual(10);
+        expect(multiply(2,5)).toEqual(10);
+        expect(multiply(1000,4)).toEqual(4000);
+        expect(multiply(1,9)).toEqual(9);
+        expect(multiply(6,7)).toEqual(42);
+    });
+    it('should reject invalid argument types', function() {
+        expect(function() {
+           multiply('a', 'b');
+        }).toThrow("Please provide only integers to function:multiply");
+
+        expect(function() {
+            multiply(true, NaN);
+         }).toThrow("Please provide only integers to function:multiply");
+    });
+})
